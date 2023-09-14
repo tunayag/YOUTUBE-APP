1c6eae7cd21818042629f1123885830c04498146
tunayag <113175657+tunayag@users.noreply.github.com>
2023-09-14 23:26:57 +0300

    updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,27 +10,39 @@ function Feed() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items));
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
   }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
         <Sidebar
           selectedCategory={selectedCategory}
           setSelectedCategory={setSelectedCategory}
         />
 
         <Typography variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
-          © 2022 JSM Media
+          © 2023
         </Typography>
       </Box>
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
           {selectedCategory}
-           <span style={{ color: "#FC1503" }}> videos</span>
+          <span style={{ color: "#FC1503" }}> videos</span>
         </Typography>
 
         <Videos videos={videos} />
